ad6aeeb4e2ffb6105ac939240ccb3bee93c12945
Sandra Liu <sara@frad.in>
2022-09-06 15:55:05 +0200

    Installed Pinia and created store. Set data values. Added getters: Get inifinitives of verbs. Get the pronouns of a chosen verb. Added actions: Get random verb from verbs object (JSON file).

---
+++ src/store/verbs.js
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia'
+
+export const useVerbsStore = defineStore('verbs', {
+  state: () => ({
+    verbsObject: {},
+    chosenVerb: "",
+  }),
+  getters: {
+    /**
+     * Gets verbs' infinitives and maps them to start with upper case.
+     * @returns {Array<String>} List of verbs.
+     */
+    getVerbsInfinitives (state) {
+      const getVerbs = Object.keys(state.verbsObject);
+      return getVerbs.map((verb) => {
+        return verb[0].toUpperCase() + verb.substring(1);
+      })
+    },
+
+    /**
+   * Gets the keys (pronouns) of randomly chosen verb.
+   * @type {String}
+   */
+    displayPronouns (state) {
+      if (!state.chosenVerb) {
+        return [];
+      }
+      const pronouns = Object.keys(state.verbsObject[state.chosenVerb.toLowerCase()]);
+      return pronouns.map((pronoun) => {
+        return pronoun[0].toUpperCase() + pronoun.substring(1);
+      })
+    }
+  },
+
+  actions: {
+    setVerbs (newVerbsObject) {
+      this.verbsObject = newVerbsObject;
+    },
+
+    setChosenVerb (newChosenVerb) {
+      this.chosenVerb = newChosenVerb;
+    },
+
+    /**
+     * Gets the total number of verbs (from the array).
+     * @returns {String} Ranomly chosen verb.
+     */
+    getRandomVerb () {
+      const objectsLength = this.getVerbsInfinitives.length;
+      return this.getVerbsInfinitives[Math.floor(Math.random() * objectsLength)];
+    },
+  },
+})
