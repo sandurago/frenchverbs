6b5524e48cb0eb73a9d037b75d3261553f6771be
Sandra Liu <sara@frad.in>
2022-09-06 15:58:23 +0200

    Moved a part of the code to App.vue after creating Pinia store: -> moved method that gets data, its  JSON format and assigns it to a variable, -> moved created that sets randomly chosen verb.

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,33 @@
   </nav>
   <router-view />
 </template>
+
+<script>
+import { useVerbsStore } from '@/store/verbs';
+import { mapStores } from 'pinia';
+
+export default {
+  computed: {
+    ...mapStores(useVerbsStore),
+  },
+
+  methods: {
+    /**
+   * Gets the verbs.json file.
+   * Converts the file to JSON format.
+   * Assigns it to data variable.
+   *  @type {Object}
+   */
+    async getData () {
+      const fetchFile = await fetch('/verbs.json');
+      const getJson = await fetchFile.json();
+      this.verbsStore.setVerbs(getJson);
+    },
+  },
+
+  async created () {
+    await this.getData();
+    this.verbsStore.setChosenVerb(this.verbsStore.getRandomVerb());
+  }
+}
+</script>
