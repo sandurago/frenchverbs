3248962d0dd3a89a5aa36ea9f279e2238fc91d2d
Sandra Liu <sara@frad.in>
2023-02-25 20:19:01 +0100

    Created component and fetched json.

---
+++ src/store/vocab.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia';
+
+export const useVocabStore = defineStore('vocab', {
+  state: () => ({
+
+  }),
+
+  getters: {
+
+  },
+
+  actions: {
+    
+  }
+})
---
+++ src/views/VocabularyPractice.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span>Choose the correct translation:</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    vocabArray: [],
+  }),
+
+  methods: {
+    /**
+     * Gets array with vocabulary.
+     */
+    async getVocabulary () {
+      const fetchFile = await fetch('/vocabulary.json');
+      const getJson = await fetchFile.json();
+      this.setVocabulary(getJson);
+    },
+
+    /**
+     * Fills in data in the component with content from json.
+     * @param {Array<Object>} newVocabArray 
+     */
+    setVocabulary(newVocabArray) {
+      this.vocabArray = newVocabArray;
+    }
+  },
+
+  created() {
+    this.getVocabulary();
+  }
+}
+</script>
